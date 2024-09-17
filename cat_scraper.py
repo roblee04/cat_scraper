1491b8ef8ae263b30ed9010a54036a12f107af71
Robin <roblee04@gmail.com>
2024-09-17 14:44:05 -0700

    cat pix scraped

--- cat_scraper.py
+++ cat_scraper.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
+
 
 # funnycatpix.com
 # catoftheday.com
@@ -10,7 +12,7 @@ from bs4 import BeautifulSoup
 def main():
 
     # Where we put the stored data
-    DIR_PATH = sys.argv[1]
+    # DIR_PATH = sys.argv[1]
     
     src = "https://www.funnycatpix.com"
     url = src + '/page/'
@@ -19,29 +21,33 @@ def main():
     # prevent duplicates by tracking most recent post
     # most recent entry, 
     
+    with open("urls.txt", "a") as f:
+        for idx in tqdm(range(1, max_idx +1)):
 
-    for idx in range(max_idx, max_idx + 2):
+            # Remake every url header and request the page
+            new_url = url + str(idx)
+            response = requests.get(new_url)
+            html_content = response.content
 
-        # Remake every url header and request the page
-        url = url + str(idx)
-        response = requests.get(url)
-        html_content = response.content
+            # make soup object
+            soup = BeautifulSoup(html_content, 'html.parser')
 
-        # make soup object
-        soup = BeautifulSoup(html_content, 'html.parser')
+            # try to scrape for images, if we cannot, return
+            try:
+                posts = soup.find("section",id="posts")
+                image_tags = posts.find_all('img')
+                for image_tag in image_tags:
+                    image_url = image_tag['src']
+                    image_name = image_tag['alt']
 
-        # try to scrape for images, if we cannot, return
-        try:
-            posts = soup.find("section",id="posts")
-            image_tags = posts.find_all('img')
-            for image_tag in image_tags:
-                image_url = image_tag['src']
-                image_name = image_tag['alt']
-                
-                print(image_name, image_url, DIR_PATH + image_url.split("/")[-1])
+                    complete_url = src + image_url
+                    f.write(complete_url + "\n")
+                    # print(image_name, image_url, DIR_PATH + image_url.split("/")[-1])
 
-        except AttributeError:
-            print("Last Page Reached. Page " + str(idx - 1))
+            except AttributeError:
+                print(url)
+                print("Last Page Reached. Page " + str(idx - 1))
+
         
 
 if __name__ == "__main__":
